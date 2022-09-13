0560c4a3595ba8cf295fac7856b44aa186ee9624
Frenkie Leo <102043631+FrenkieLeo@users.noreply.github.com>
2022-09-14 00:26:07 +0800

    Update init.js

--- model/init.js
+++ model/init.js
@@ -3,9 +3,9 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 // // 从环境变量中读取数据库配置
 // const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
-const MYSQL_ADDRESS = 'sh-cynosdbmysql-grp-as2uyv3y.sql.tencentcdb.com:20531';
-const MYSQL_USERNAME = 'root';
-const MYSQL_PASSWORD ='FZbuk3nb';
+const MYSQL_ADDRESS = 'sh-cynosdbmysql-grp-nhbmhflq.sql.tencentcdb.com:20563';
+const MYSQL_USERNAME = 'car_admin';
+const MYSQL_PASSWORD ='Admin@!@#';
 
 const [host, port] = MYSQL_ADDRESS.split(":");
 
